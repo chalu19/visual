ef2809f103e3da0a13bcd5883a4e3975010d9421
chalu19 <54372550+chalu19@users.noreply.github.com>
2019-08-21 14:28:34 -0400

    Add files via upload

---
+++ Example1-HW.js
@@ -0,0 +1,13 @@
+﻿//Title:    Hello World 
+//Date:     6.19.2019
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res)
+    {
+        res.send('Hello World')
+    }
+)  
+
+app.listen(3000)
